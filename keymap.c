83ce85e492ec4308da623a1796dc0208ccac7b87
Phong Nguyen <nhphong1406@gmail.com>
2018-08-03 12:22:30 +0700

    Adds mouse keys

--- keymap.c
+++ keymap.c
@@ -18,7 +18,7 @@ enum custom_layers {
  * |-----------------------------------------------------------|
  * |Shft   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|  \| Up|Sft|
  * |-----------------------------------------------------------|
- * |   ||Ctl|Alt|Cmd|   |   Spc   |Bsp|   |   |   ||Lft|Dwn|Rgh|
+ * |   ||Ctl|Alt|Cmd|Ent|   Spc   |Bsp|   |   |   ||Lft|Dwn|Rgh|
  * `-----------------------------------------------------------'
  */
 
@@ -40,9 +40,9 @@ enum custom_layers {
  * ,-----------------------------------------------------------.
  * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
  * |-----------------------------------------------------------|
- * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
+ * |     |   |Mcl|MU |Rcl|   |   |Rcl|WU |Mcl|   |   |   |     |
  * |------------------------------------------------------`    |
- * |      |   |   |   |   |   |   |   |   |   |   |   |   |    |
+ * |      |   |ML |MD |MR |Lcl|Lcl|WL |WD |WR |   |   |   |    |
  * |-----------------------------------------------------------|
  * |       |   |   |   |   |   |   |   |   |   |   |   |   |   |
  * |-----------------------------------------------------------|
@@ -66,11 +66,11 @@ enum custom_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_JP(
-        KC_ESC,             KC_1,          KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,      KC_0,                KC_MINS, KC_EQL,   KC_BSLS, KC_BSPC,
-        KC_TAB,             KC_Q,          KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,                KC_GRV,  KC_LBRC,
-        KC_LCTL,            KC_A,          KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,      LT(_MOUSE, KC_SCLN), KC_QUOT, KC_RBRC,  KC_ENT,
-        KC_LSFT,            KC_Z,          KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH,             KC_BSLS, KC_UP,    KC_RSFT,
-        LT(_FUNC, KC_GRV),  CTL_T(KC_GRV), KC_LALT, KC_LGUI, KC_ENT,  KC_SPC, KC_BSPC, KC_RGUI, KC_RALT, MO(_FUNC),                      KC_LEFT, KC_DOWN,  KC_RGHT
+        KC_ESC,            KC_1,          KC_2,    KC_3,    KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,      KC_0,                KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC,
+        KC_TAB,            KC_Q,          KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,                KC_GRV,  KC_LBRC,
+        KC_LCTL,           KC_A,          KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,      LT(_MOUSE, KC_SCLN), KC_QUOT, KC_RBRC, KC_ENT,
+        KC_LSFT,           KC_Z,          KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH,             KC_BSLS, KC_UP,   KC_RSFT,
+        LT(_FUNC, KC_GRV), CTL_T(KC_GRV), KC_LALT, KC_LGUI, KC_ENT, KC_SPC, KC_BSPC, KC_RGUI, KC_RALT, MO(_FUNC),                      KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FUNC] = LAYOUT_JP(
         KC_PWR,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
@@ -80,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______
     ),
     [_MOUSE] = LAYOUT_JP(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, KC_BTN3, KC_MS_U, KC_BTN2, _______, _______, KC_BTN2, KC_WH_U, KC_BTN3, _______, _______, _______,
+        _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1, KC_BTN1, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______
     )
