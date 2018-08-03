48ab6a66765aa0ab3b5365cdcd3d8e9ecbac12fe
Phong Nguyen <nhphong1406@gmail.com>
2018-08-03 13:39:02 +0700

    Maps Right CTRL

--- keymap.c
+++ keymap.c
@@ -9,7 +9,7 @@ enum custom_layers {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Layer 0: HHKB JP (custom)
+    /* Layer 0: HHKB JP Base
     * ,-----------------------------------------------------------.
     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9| 10|  -|  =| \ |Bsp|
     * |-----------------------------------------------------------|
@@ -23,14 +23,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `-----------------------------------------------------------'
     */
     [_BASE] = LAYOUT_JP(
-        KC_ESC,            KC_1,          KC_2,    KC_3,    KC_4,               KC_5,   KC_6,                KC_7,    KC_8,    KC_9,      KC_0,                KC_MINS,        KC_EQL,  KC_BSLS, KC_BSPC,
-        KC_TAB,            KC_Q,          KC_W,    KC_E,    KC_R,               KC_T,   KC_Y,                KC_U,    KC_I,    KC_O,      KC_P,                KC_GRV,         KC_LBRC,
-        CTL_T(KC_ESC),     KC_A,          KC_S,    KC_D,    KC_F,               KC_G,   KC_H,                KC_J,    KC_K,    KC_L,      LT(_MOUSE, KC_SCLN), GUI_T(KC_QUOT), KC_RBRC, KC_ENT,
-        KC_LSFT,           KC_Z,          KC_X,    KC_C,    KC_V,               KC_B,   KC_N,                KC_M,    KC_COMM, KC_DOT,    KC_SLSH,             KC_BSLS,        KC_UP,   KC_RSFT,
-        LT(_FUNC, KC_GRV), CTL_T(KC_GRV), KC_LALT, KC_LGUI, LT(_MOUSE, KC_ENT), KC_SPC, LT(_MOUSE, KC_BSPC), KC_RGUI, KC_RALT, MO(_FUNC),                      KC_LEFT,        KC_DOWN, KC_RGHT
+        KC_ESC,            KC_1,          KC_2,    KC_3,    KC_4,               KC_5,   KC_6,                KC_7,    KC_8,    KC_9,      KC_0,                KC_MINS,         KC_EQL,  KC_BSLS, KC_BSPC,
+        KC_TAB,            KC_Q,          KC_W,    KC_E,    KC_R,               KC_T,   KC_Y,                KC_U,    KC_I,    KC_O,      KC_P,                KC_GRV,          KC_LBRC,
+        CTL_T(KC_ESC),     KC_A,          KC_S,    KC_D,    KC_F,               KC_G,   KC_H,                KC_J,    KC_K,    KC_L,      LT(_MOUSE, KC_SCLN), GUI_T(KC_QUOT),  KC_RBRC, KC_ENT,
+        KC_LSFT,           KC_Z,          KC_X,    KC_C,    KC_V,               KC_B,   KC_N,                KC_M,    KC_COMM, KC_DOT,    KC_SLSH,             RCTL_T(KC_BSLS), KC_UP,   KC_RSFT,
+        LT(_FUNC, KC_GRV), CTL_T(KC_GRV), KC_LALT, KC_LGUI, LT(_MOUSE, KC_ENT), KC_SPC, LT(_MOUSE, KC_BSPC), KC_RGUI, KC_RALT, MO(_FUNC),                      KC_LEFT,         KC_DOWN, KC_RGHT
     ),
 
-    /* Layer 1: HHKB FN Mode
+    /* Layer 1: HHKB Fn Mode
     * ,-----------------------------------------------------------.
     * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
     * |-----------------------------------------------------------|
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_DEL,  _______, _______
     ),
 
-    /* Layer 2: Mouse Mode
+    /* Layer 2: HHKB Mouse Mode
     * ,-----------------------------------------------------------.
     * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
     * |-----------------------------------------------------------|
