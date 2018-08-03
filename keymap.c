36b25294a376c24438fa6b6d2eac6e934af44f2a
Phong Nguyen <nhphong1406@gmail.com>
2018-08-03 14:16:21 +0700

    Adds function keys to Mouse layers

--- keymap.c
+++ keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,            KC_1,          KC_2,    KC_3,    KC_4,               KC_5,   KC_6,                KC_7,    KC_8,    KC_9,      KC_0,                KC_MINS,         KC_EQL,  KC_BSLS, KC_BSPC,
         KC_TAB,            KC_Q,          KC_W,    KC_E,    KC_R,               KC_T,   KC_Y,                KC_U,    KC_I,    KC_O,      KC_P,                KC_GRV,          KC_LBRC,
         CTL_T(KC_ESC),     KC_A,          KC_S,    KC_D,    KC_F,               KC_G,   KC_H,                KC_J,    KC_K,    KC_L,      LT(_MOUSE, KC_SCLN), GUI_T(KC_QUOT),  KC_RBRC, KC_ENT,
-        KC_LSFT,           KC_Z,          KC_X,    KC_C,    KC_V,               KC_B,   KC_N,                KC_M,    KC_COMM, KC_DOT,    KC_SLSH,             RCTL_T(KC_BSLS), KC_UP,   KC_RSFT,
+        KC_LSFT,           KC_Z,          KC_X,    KC_C,    KC_V,               KC_B,   KC_N,                KC_M,    KC_COMM, KC_DOT,    RCTL_T(KC_SLSH),     RSFT_T(KC_BSLS), KC_UP,   KC_RSFT,
         LT(_FUNC, KC_GRV), CTL_T(KC_GRV), KC_LALT, KC_LGUI, LT(_MOUSE, KC_ENT), KC_SPC, LT(_MOUSE, KC_BSPC), KC_RGUI, KC_RALT, MO(_FUNC),                      KC_LEFT,         KC_DOWN, KC_RGHT
     ),
 
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Layer 2: HHKB Mouse Mode
     * ,-----------------------------------------------------------.
-    * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+    * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |   |
     * |-----------------------------------------------------------|
     * |     |   |Mcl|MU |Rcl|   |   |Rcl|WU |Mcl|   |   |   |     |
     * |------------------------------------------------------`    |
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `-----------------------------------------------------------'
     */
     [_MOUSE] = LAYOUT_JP(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11, KC_F12,  _______, _______,
         _______, _______, KC_BTN3, KC_MS_U, KC_BTN2, _______, _______, KC_BTN2, KC_WH_U, KC_BTN3, _______, _______, _______,
         _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1, KC_BTN1, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
