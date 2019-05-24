3dca3143756e53e053575c648abdbe3437c86d60
Phong Nguyen <nhphong1406@gmail.com>
2019-05-24 22:49:32 +0700

    Re-position some keys: \, Up and Shift

--- keymap.c
+++ keymap.c
@@ -13,11 +13,11 @@ enum hhkb_layers {
 #define CTL_Z     CTL_T(KC_Z)
 #define CTL_SLS   CTL_T(KC_SLSH)
 #define GUI_QUO   GUI_T(KC_QUOT)
-#define SFT_UP    RSFT_T(KC_UP)
 #define MOU_SCL   LT(_MOUSE, KC_SCLN)     // Turn on _MOUSE layer when held, ; when tapped
 #define LWR_SPC   LT(_LOWER, KC_SPC)      // Turn on _LOWER layer when held, Space when tapped
 #define RSE_GRV   LT(_RAISE, KC_GRV)      // Turn on _RAISE layer when held, ` when tapped
 #define RSE_BSP   LT(_RAISE, KC_BSPC)     // Turn on _RAISE layer when held, Backspace when tapped
+#define RSE_BSL   LT(_RAISE, KC_BSLS)     // Turn on _RAISE layer when held, Backslash when tapped
 #define SAV_PIC   LSFT(LGUI(KC_4))        // Shift+Command+4: Save picture of selected area to the clipboard (macOS only)
 #define COP_PIC   LCTL(LSFT(LGUI(KC_4)))  // Ctrl+Shift+Command+4: Copy picture of selected area to the clipboard (macOS only)
 
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
         CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    MOU_SCL, GUI_QUO, KC_GRV,  KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_SLS, KC_BSLS, SFT_UP,  KC_RSFT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_SLS, KC_RSFT, KC_UP,   RSE_BSL,
         RSE_GRV, CTL_GRV, KC_LALT, KC_LGUI, LWR_SPC,     KC_SPC,      RSE_BSP, KC_RGUI, KC_RALT, MOUSE,   KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, KC_PIPE, KC_DEL,
         _______, XXXXXXX, KC_MS_U, KC_BTN3, KC_BTN2, XXXXXXX, XXXXXXX, KC_BTN2, KC_WH_U, KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX,
         _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1, XXXXXXX, KC_BTN1, KC_WH_L, KC_WH_D, KC_WH_R, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, AG_NORM, AG_SWAP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, RESET,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, AG_NORM, AG_SWAP, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, RESET,
         _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______, _______, _______
     ),
 
